ca6ad33f5f74bd05222e8ff17092b0ac9d05a6d8
FINT\FINT <finthak@gmail.com>
2025-02-11 15:19:34 +0200

    next update 0.3

--- app.ts
+++ app.ts
@@ -132,3 +132,30 @@ const enum RolesTwo {
 }
 
 const res2 = RolesTwo.ADMIN;
+
+    //next lessons function //
+
+enum StatusCodeThre {
+    Published = 'published',
+    Draft = 'draft',
+    Deleted = 'deleted'
+}
+
+async function getFaqs(req: {
+    topicId: number;
+    status: StatusCodeThre
+}): Promise<{
+        question: string,
+        answer: string,
+        tags:string [],
+        likes: number,
+        status: StatusCodeThre
+    } [] > {
+	const res = await fetch('/faqs', {
+		method: 'POST',
+		body: JSON.stringify(req)
+	});
+	const data = await res.json();
+	return data;
+}
+
