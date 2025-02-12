79d76ac4521363b1925689a95a8509105aff00be
FINT\FINT <finthak@gmail.com>
2025-02-12 17:51:24 +0200

    next update v 0.3

--- app.js
+++ app.js
@@ -111,3 +111,40 @@ function getFaqs(req) {
         return data;
     });
 }
+//next lessons union звуження типів //
+function logId(id) {
+    if (typeof id === 'string') {
+        console.log(id);
+    }
+    else if (typeof id === 'number') {
+        console.log(id);
+    }
+    else {
+        console.log(id);
+    }
+}
+logId(1);
+function logError(err) {
+    if (Array.isArray(err)) {
+        console.log(err);
+    }
+    else {
+        console.log(err);
+    }
+}
+function logObject(obj) {
+    if ('a' in obj) {
+        console.log(obj.a);
+    }
+    else {
+        console.log(obj.b);
+    }
+}
+function logMultipleIds(a, b) {
+    if (a === b) {
+    }
+    else {
+        console.log(a);
+    }
+}
+//next lessons Literal Types //
--- app.ts
+++ app.ts
@@ -159,3 +159,46 @@ async function getFaqs(req: {
 	return data;
 }
 
+    //next lessons union звуження типів //
+
+    function logId(id: string | number | boolean){
+        if (typeof id === 'string'){
+            console.log(id);
+        } else if ( typeof id === 'number'){
+            console.log(id); 
+        } else {
+            console.log(id);
+        }
+    }
+
+    logId(1);
+
+function logError(err:string | string[]){
+    if (Array.isArray(err)){
+        console.log(err);
+    } else {
+        console.log(err);
+    }
+}
+
+
+function logObject(obj: { a : number} | { b : number} ) {
+    if('a' in obj) {
+        console.log(obj.a);
+    } else {
+        console.log(obj.b);
+    }
+}
+
+
+function logMultipleIds(a:string |number, b : string |boolean) {
+    if (a === b){
+    
+    } else {
+        console.log(a);
+    }
+}
+
+  //next lessons Literal Types //
+
+  
